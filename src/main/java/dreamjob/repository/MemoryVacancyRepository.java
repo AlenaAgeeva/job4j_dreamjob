e3aa1ebe7841ee9361b3919f4ed330059bfe8e4b
Alena Ageeva <alenaageevapv@gmail.com>
2024-07-28 13:07:45 -0300

    4. Thymeleaf, Циклы. [#504841 #506718]

--- src/main/java/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/dreamjob/repository/MemoryVacancyRepository.java
@@ -2,6 +2,7 @@ package dreamjob.repository;
 
 import dreamjob.model.Vacancy;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,12 +17,12 @@ public class MemoryVacancyRepository implements VacancyRepository {
     private final Map<Integer, Vacancy> vacancies = new HashMap<>();
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer"));
-        save(new Vacancy(0, "Junior Java Developer"));
-        save(new Vacancy(0, "Junior+ Java Developer"));
-        save(new Vacancy(0, "Middle Java Developer"));
-        save(new Vacancy(0, "Middle+ Java Developer"));
-        save(new Vacancy(0, "Senior Java Developer"));
+        save(new Vacancy(0, "Intern Java Developer", "text", LocalDateTime.now()));
+        save(new Vacancy(1, "Junior Java Developer", "text", LocalDateTime.now()));
+        save(new Vacancy(2, "Junior+ Java Developer", "text", LocalDateTime.now()));
+        save(new Vacancy(3, "Middle Java Developer", "text", LocalDateTime.now()));
+        save(new Vacancy(4, "Middle+ Java Developer", "text", LocalDateTime.now()));
+        save(new Vacancy(5, "Senior Java Developer", "text", LocalDateTime.now()));
     }
 
     public static MemoryVacancyRepository getInstance() {
@@ -43,7 +44,10 @@ public class MemoryVacancyRepository implements VacancyRepository {
     @Override
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(),
-                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle())) != null;
+                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(),
+                        vacancy.getTitle(),
+                        vacancy.getDescription(),
+                        vacancy.getCreationDate())) != null;
     }
 
     @Override
